74864269809c706d65565d6db6156e9ae3099d21
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-01 08:44:29 -0300

    modify loop principal
    modifiquei o loop pricipal para do while ao inves de while
    obs: foi concertado o erro da saida do jogo

--- rpg/Principal.java
+++ rpg/Principal.java
@@ -10,48 +10,46 @@ public class Principal extends Sistema {
         Sistema sistema = new Sistema();
         Heroi heroi = new Heroi();
         Enemy inimigo = new Enemy();
- 
-     
-        while (heroi.estaVivo() && inimigo.estaVivo()){
 
-            
-            String opcao = JOptionPane.showInputDialog("\n|Escolha uma ação| \n|1| Atacar \n|2| Curar" +
-            "\n|3| Sair do Jogo");
+        int opcao;
+
+        do{
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("\n|Escolha uma ação| \n|1| Atacar \n|2| Curar" +
+            "\n|3| Sair do Jogo"));
 
             int indiceDano = aleatorio.nextInt(sistema.efeitoDano.length);
             int indiceAtaque = aleatorio.nextInt(sistema.efeitoAtaque.length);
 
-            switch (opcao) { 
-                case "1":
-                inimigo.setVida(inimigo.getVida() - heroi.getDanoAtaque());
-                JOptionPane.showMessageDialog(null, sistema.efeitoAtaque[indiceAtaque]);
-                inimigo.status(inimigo.getVida(), inimigo.getName()); 
-                    break;
-                case "2":
-                heroi.usarPorçao();
-                JOptionPane.showMessageDialog(null, "Após tomar a poção com coragem, seu personagem sente um poder revitalizante \npercorrer suas veias, renovando sua força e determinação.");
-                heroi.status(heroi.getVida(), heroi.getName());
-                    break;
-                case "3":
-                JOptionPane.showMessageDialog(null,"Aperte em OK para sair do Jogo");
-                    break;
-                default:
-                    break;
-            }
-
-            JOptionPane.showMessageDialog(null, sistema.efeitoDano[indiceDano]);
-
-            heroi.setVida(heroi.getVida() - inimigo.getDanoAtaque());
-
-            JOptionPane.showMessageDialog(null, "voce perdeu " + heroi.getDanoAtaque() + heart + "de vida");
-
-            heroi.status(heroi.getVida(), heroi.getName());
-            
-
+    
+                switch (opcao) { 
+                    case 1:
+    
+                    inimigo.setVida(inimigo.getVida() - heroi.getDanoAtaque());
+                    JOptionPane.showMessageDialog(null, sistema.efeitoAtaque[indiceAtaque]);
+                    inimigo.status(inimigo.getVida(), inimigo.getName()); 
+                    JOptionPane.showMessageDialog(null, sistema.efeitoDano[indiceDano]);
+    
+                    heroi.setVida(heroi.getVida() - inimigo.getDanoAtaque());
+    
+                    JOptionPane.showMessageDialog(null, "voce perdeu " + heroi.getDanoAtaque() + heart + "de vida");
+    
+                    heroi.status(heroi.getVida(), heroi.getName());
+                        break;
+                    case 2:
+                    heroi.usarPorçao();
+                    JOptionPane.showMessageDialog(null, "Após tomar a poção com coragem, seu personagem sente um poder revitalizante \npercorrer suas veias, renovando sua força e determinação.");
+                    heroi.status(heroi.getVida(), heroi.getName());
+                        break;
+                    case 3:
+                    JOptionPane.showMessageDialog(null,"Aperte em OK para sair do Jogo");
+                        break;
+                    default:
+                        break;
+                }
+         
             JOptionPane.showMessageDialog(null, "Rodada Finalizada");
-
-        }
-
+            
+        }while(heroi.estaVivo() && inimigo.estaVivo() && opcao != 3);
     }      
 
 
