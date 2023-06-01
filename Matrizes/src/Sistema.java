57134af0371dc87f01b233cd32bf64907d3b9f5c
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-01 03:16:26 -0300

    codigo refatorado

--- Matrizes/src/Sistema.java
+++ Matrizes/src/Sistema.java
@@ -8,23 +8,6 @@ public class Sistema {
     
     private int matriz [][] = new int[3][3];
 
-    /* public void trezXtrez(){
-
-        int auxiliar = aleatorio.nextInt(25);
-
-        matriz[0][0] = auxiliar;
-        matriz[0][1] = auxiliar;
-        matriz[0][2] = auxiliar;
-    
-        matriz[1][0] = auxiliar;
-        matriz[1][1] = auxiliar;
-        matriz[1][2] = auxiliar;
-    
-        matriz[2][0] = auxiliar;
-        matriz[2][1] = auxiliar;
-        matriz[2][2] = auxiliar; 
-    } */
-
     public void trezXtrez() {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
