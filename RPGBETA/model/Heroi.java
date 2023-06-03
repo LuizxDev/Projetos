22804886f04b8497d0eccba6c843242c6c19ba4c
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-02 21:17:41 -0300

    metodo de ataque do inimigo

--- RPGBETA/model/Heroi.java
+++ RPGBETA/model/Heroi.java
@@ -2,13 +2,15 @@ package RPGBETA.model;
 
 public class Heroi extends Personagens {
 
-    /*public Heroi(String nome) {
-        this.setNome(nome);
-    }*/
 
     public int usarPorcao(Heroi heroi){
         heroi.setVida(getVida() - 10);
         return heroi.getVida();
+    }
+
+    public int ataque(Inimigo inimigo){
+        inimigo.setVida(getVida() - this.getDanoAtaque());
+        return inimigo.getVida();
     }
 
     public boolean estaVivo() {
--- RPGBETA/view/Aplicacao.java
+++ RPGBETA/view/Aplicacao.java
@@ -9,10 +9,26 @@ public class Aplicacao {
 
         controller ctrl = new controller();
 
-        int escolha = Integer.parseInt(JOptionPane.showInputDialog("\n|Escolha uma ação| \n|1| Atacar \n|2| Curar" +
+        int escolha = Integer.parseInt(JOptionPane.showInputDialog("\n|Escolha uma ação| \n|1| Atacar \n|2| Usar Porção" +
         "\n|3| Sair do Jogo"));
 
         ctrl.IniciarJogo(escolha);
+
+        switch (escolha) {
+            case 1:
+                
+                break;
+            case 2:
+            //usar poçao
+            JOptionPane.showMessageDialog(null, "Após tomar a poção com coragem, seu personagem sente um poder revitalizante \npercorrer suas veias, renovando sua força e determinação.");
+
+                break;
+            case 3:
+                break;
+        
+            default:
+                break;
+        }
       
     }
 }
